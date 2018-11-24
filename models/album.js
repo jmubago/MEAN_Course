e7ca95d65539de3899a90ec444b803c6c31ffbca
jmubago <jmubagosotomayor@gmail.com>
2018-11-24 18:58:56 +0100

    Added middleware for removing

--- models/album.js
+++ models/album.js
@@ -1,6 +1,8 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var Song = require('./song');
+
 var schema = mongoose.Schema;
 
 var albumSchema = schema({
@@ -10,5 +12,12 @@ var albumSchema = schema({
     image: String,
     artist: { type: schema.ObjectId, ref: 'Artist' },
 });
+
+// delete all related songs
+albumSchema.pre('remove', (next) => {
+    Song.remove({album: this._id}).exec();
+    next();
+})
+
 
 module.exports = mongoose.model('Album', albumSchema);
